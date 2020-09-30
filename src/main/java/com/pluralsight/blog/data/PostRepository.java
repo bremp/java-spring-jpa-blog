d0262c2bd4e9b64b138315fbfae0782a20d57141
Bryan Yadao <bryan.g.yadao@saic.com>
2020-09-30 08:20:54 -1000

    Completed module

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,9 +1,13 @@
 package com.pluralsight.blog.data;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+  List<Post> findByCategory(Category category);
 }
